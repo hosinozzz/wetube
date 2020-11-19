37e34fcd4eb72a2662e23dd1c146a745c900feea
hosinoz <hosinozzz@gmail.com>
2020-11-19 14:55:11 +0900

    #2.4 Handling Routes with Express

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ function handleListening() {
     console.log(`Listening on: http://localhost:${PORT}`);
 }
 
+function handleHome(req, res) {
+    res.send("hi cool sexy");
+}
+
+function handleProfile(req, res) {
+    res.send("you are on my sexy cool guy")
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
 // app.get('/', function (req, res) {
 //   res.send('Hello World')
