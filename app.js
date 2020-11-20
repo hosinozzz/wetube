9021b6a7a88d5583a7b45d62dd81bac338504782
hosinoz <hosinozzz@gmail.com>
2020-11-20 11:54:07 +0900

    #2.10 MVC Pattern part Two

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import bodyParser from "body-parser";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 
 
@@ -21,8 +22,8 @@ app.use(helmet());
 app.use(morgan("dev"));
 
 
-app.use("/", globalRouter);
-app.use("/user", userRouter)
-app.use("/video", videoRouter)
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter)
+app.use(routes.videos, videoRouter)
 
 export default app;   
