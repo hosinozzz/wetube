9a926a60bfd7562472154c923eb69e117d007b9c
hosinoz <hosinozzz@gmail.com>
2020-12-01 00:07:56 +0900

    #2.22 Home Controller part Two

---
+++ db.js
@@ -0,0 +1,43 @@
+export const videos = [
+  {
+    id: 1111,
+    title: "ero video",
+    description: "mecya ero i desu",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 2222,
+      name: "hosinoz",
+      email: "ero@gmail.com",
+    },
+  },
+
+  {
+    id: 123123123,
+    title: "ero video??????????",
+    description: "mecya ero i desu!!!!!!!",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 2222,
+      name: "hosinoz",
+      email: "ero@gmail.com",
+    },
+  },
+
+  {
+    id: 1241414124,
+    title: "ero video!!!!!!!!!",
+    description: "mecya ero i desu!!!!!!",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 2222,
+      name: "hosinoz",
+      email: "ero@gmail.com",
+    },
+  },
+];
