a65fd3d4d90b9dd862ae35f7f5adc9b16a826855
hosinoz <hosinozzz@gmail.com>
2021-02-17 13:06:50 +0900

    #3.5 Home Controller Finished

--- models/Comment.js
+++ models/Comment.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const CommentSchema = new.mongoose.Schema({
+const CommentSchema = new mongoose.Schema({
   text: {
     type: String,
     required: "Text is required",
