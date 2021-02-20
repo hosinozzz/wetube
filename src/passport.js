9de1aeefe02b894bd915f8a8594477dac3cd50ce
hosinoz <hosinozzz@gmail.com>
2021-02-20 22:41:31 +0900

    passport #10

--- src/passport.js
+++ src/passport.js
@@ -15,9 +15,8 @@ passport.use(
     {
       clientID: process.env.GH_ID,
       clientSecret: process.env.GH_SECRET,
-      callbackURL: process.env.PRODUCTION
-        ? `https://whispering-harbor-25192.herokuapp.com/${routes.githubCallback}`
-        : `http://localhost:4000${routes.githubCallback}`,
+      callbackURL: `https://whispering-harbor-25192.herokuapp.com/auth/github/callback`,
+      proxy: true,
     },
     githubLoginCallback
   )
